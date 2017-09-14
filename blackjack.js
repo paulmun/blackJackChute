77cd305039ec27026f613359d49493b31b9630cb
Paul Mun <paulmun818@gmail.com>
2017-09-13 23:59:24 -0400

    deckbuilding logic

---
+++ blackjack.js
@@ -0,0 +1,21 @@
+var players = {
+    "house": [],
+    "player1": [],
+}
+
+var addition = function(hand) {
+    var total = 0;
+    for (var i = 0; i < hand.length; i++) {
+        switch (hand[i][0]) {
+            case "A":
+                break;
+            case "J":
+                break;
+            case "Q":
+                break;
+            case "K":
+                break;
+            default:
+        }
+    }
+}
